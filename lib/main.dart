35081d5be9b2c89f6ed89adebb216867c0f2a687
takashi11171117 <takashi11171117@gmail.com>
2023-08-04 18:54:02 +0900

    白黒変換とグレースケール変換を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+import 'package:image/image.dart' as img;
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter_tesseract_ocr/flutter_tesseract_ocr.dart';
-import 'dart:io';
 import 'package:flutter/foundation.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -53,19 +56,58 @@ class MyHomePageState extends State<MyHomePage> {
       print("Please select language");
       return;
     }
-    path = url;
-    var langs = selectList.join("+");
 
     loading = true;
     setState(() {});
 
+    img.Image? image = img.decodeImage(File(url).readAsBytesSync());
+    if (image == null) return;
+
+    final newPath = convertBinaryColor(image, url);
+
+    path = newPath;
+    String langs = selectList.join("+");
+
     _ocrText =
-        await FlutterTesseractOcr.extractText(url, language: langs, args: {
+        await FlutterTesseractOcr.extractText(path, language: langs, args: {
       "preserve_interword_spaces": "1",
     });
 
     loading = false;
     setState(() {});
+  }
+
+  String convertBinaryColor(img.Image image, String path) {
+    for (var x = 0; x < image.width; x++) {
+      for (var y = 0; y < image.height; y++) {
+        var pixel = image.getPixelSafe(x, y);
+
+        num red = pixel.r;
+        num green = pixel.g;
+        num blue = pixel.b;
+
+        num luma = 0.25 * red + 0.55 * green + 0.15 * blue;
+
+        int binaryColor = luma < 150 ? 0 : 255;
+
+        image.setPixelRgba(x, y, binaryColor, binaryColor, binaryColor, 255);
+      }
+    }
+    String bwPath = p.join(p.dirname(path), "bw_${p.basename(path)}");
+    File(bwPath).writeAsBytesSync(img.encodePng(image));
+
+    return bwPath;
+  }
+
+  Future<String> convertGrayScale(img.Image image, String path) async {
+    var grayscale = img.grayscale(image);
+
+    final tempDir = await getTemporaryDirectory();
+    final grayPath = '${tempDir.path}/grayscale.png';
+    final grayFile = File(grayPath);
+    await grayFile.writeAsBytes(img.encodePng(grayscale));
+
+    return grayPath;
   }
 
   @override
